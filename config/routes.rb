9e832f9aac1533b56559e0d6ac5e064f95dd36a5
Alicia Hetrick <ahetrick22@gmail.com>
2019-04-12 14:57:32 -0400

    event handler controller is able to receive params from github issue events

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :publishing_houses
   resources :authors
   resources :books
+  resources :event_handler
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
